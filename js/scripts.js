7eaae6ce77935ce888655fb127729ae09b2779f6
Sarah Goldman <sgoldm@new-host-2.home>
2011-01-02 22:48:06 -0600

    Added homepage working files and under construction

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	var bodyWidth = $("body").width();
+	var pageWidth = 980;
+	if ( bodyWidth < pageWidth ){
+		$("body").css("background-position", "-805px 20px");
+	}
+
+	
+	
+	
+});
+
+
