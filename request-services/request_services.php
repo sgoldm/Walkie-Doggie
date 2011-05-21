400f8766ae539b1989d45e777397fdc930580f7f
Sarah Goldman <sgoldm@Sarah-Goldmans-MacBook-Pro.local>
2011-05-20 20:27:08 -0500

    Form protection

--- request-services/request_services.php
+++ request-services/request_services.php
@@ -13,7 +13,7 @@ else
 	
 	// Additional headers
 	//$headers .= "To: Walkie Doggie <info@walkiedoggie.com>" . "\r\n";
-	$headers .= "From: Walkie Doggie Site <info@walkiedoggie.com>" . "\r\n";
+	$from = "From: Walkie Doggie Site <sarahrgoldman@gmail.com>";
 	
 	$message = "<font face='Arial, Verdana, Helvetica, sans-serif' pointsize='9'>";
 	$message .= "---------------------------------------------<br>
@@ -47,8 +47,14 @@ else
 				 Initial visit availability:	" . $_POST["visit_time"] . "<br><br>
 
 				 Heard about WD from:			" . $_POST["heard_from"] . "<br>";
-	
 
+	$from = urldecode($from);
+	if (eregi("\r",$from) || eregi("\n",$from)){
+		die("Spammer detected");
+	}
+	
+	$headers .= $from;
+	
 	mail("info@walkiedoggie.com","Request for Services",$message,$headers);
 	
 	header('Location: /request-services/thank-you/');
