885d3a52e19b1fdd41d47e872fc3d980cbe06de4
Sarah Goldman <sgoldm@Sarah-Goldmans-MacBook-Pro.local>
2011-05-20 20:54:24 -0500

    Form protection

--- request-services/request_services.php
+++ request-services/request_services.php
@@ -1,6 +1,74 @@
 <?php
 
-if  (strlen($_POST["first_name"]) < 3 || strlen($_POST["last_name"]) < 3 || strlen($_POST["address"]) < 3 || strlen($_POST["city"]) < 3 || strlen($_POST["email"]) < 3)
+function validEmail($email)
+{
+   $isValid = true;
+   $atIndex = strrpos($email, "@");
+   if (is_bool($atIndex) && !$atIndex)
+   {
+      $isValid = false;
+   }
+   else
+   {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64)
+      {
+         // local part length exceeded
+         $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+         // domain part length exceeded
+         $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.')
+      {
+         // local part starts or ends with '.'
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local))
+      {
+         // local part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
+      {
+         // character not valid in domain part
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain))
+      {
+         // domain part has two consecutive dots
+         $isValid = false;
+      }
+      else if
+(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                 str_replace("\\\\","",$local)))
+      {
+         // character not valid in local part unless 
+         // local part is quoted
+         if (!preg_match('/^"(\\\\"|[^"])+"$/',
+             str_replace("\\\\","",$local)))
+         {
+            $isValid = false;
+         }
+      }
+      if ($isValid && !(checkdnsrr($domain,"MX") || 
+ ↪checkdnsrr($domain,"A")))
+      {
+         // domain not found in DNS
+         $isValid = false;
+      }
+   }
+   return $isValid;
+}
+
+$isEmail = validEmail($_POST["email"]);
+
+if  (strlen($_POST["first_name"]) < 2 || strlen($_POST["last_name"]) < 3 || strlen($_POST["address"]) < 3 || strlen($_POST["city"]) < 3 || $isEmail != true )
 { 
 	echo("Required form fields were not submitted.<br/><br/><a href='/request-services/'>Back</a>");
 }
